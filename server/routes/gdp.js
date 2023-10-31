fa40fa2609b52a1fd0fa9337892076c248300086
Marko <markolitovchenko24@gmail.com>
2023-10-31 16:58:51 -0400

    Add jsdocs

--- server/routes/gdp.js
+++ server/routes/gdp.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = express.Router()
 const { sendData, sendError, containsOnlyLetters } = require('./utils/gdp.js')
 
+/**
+ * Middleware for validating the 'country' parameter in the route
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Next middleware function
+ * @param {string} country - The 'country' parameter from the URL
+ */
 router.param('country', (req, res, next, country) => {
     if (!containsOnlyLetters(country)) {
         sendError(res, 400, 'The country name cannot contain numbers or special characters');
@@ -12,9 +20,16 @@ router.param('country', (req, res, next, country) => {
     next();
 });
 
+/**
+ * GET handler for getting the gdp of a specific country
+ * TO BE UPDATED
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 router.get('/countries/:country', (req, res) => {
     const country = req.params.country;
     sendData(res, 200, country);
-})
+});
 
 module.exports = router;
--- server/routes/utils/gdp.js
+++ server/routes/utils/gdp.js
@@ -1,12 +1,32 @@
+/**
+ * Checks if a string contains only alphabetic characters
+ *
+ * @param {string} str - The string to be checked for alphabetic characters
+ * @returns {boolean} Returns true if the string contains only alphabetic characters, false otherwise
+ */
 function containsOnlyLetters(str) {
     let regex = /^[a-zA-Z]+$/;
     return regex.test(str);
 }
 
+/**
+ * Formats the error and sends it back to the client
+ *
+ * @param {Object} res - Express response object
+ * @param {number} status - HTTP status code for the response
+ * @param {string} error - Error message to send
+ */
 function sendError(res, status, error) {
     res.status(status).send({ error: error });
 }
 
+/**
+ * Formats the data and sends it back to the client
+ *
+ * @param {Object} res - Express response object
+ * @param {number} status - HTTP status code for the response
+ * @param {any} data - Data to send
+ */
 function sendData(res, status, data) {
     res.status(status).send({ data: data });
 }
