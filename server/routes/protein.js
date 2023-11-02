a5d2b5aaa0b709728011328ceef1eb64546809d6
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-11-01 22:06:33 -0400

    Add protein api endpoints

--- server/routes/protein.js
+++ server/routes/protein.js
@@ -1,8 +1,70 @@
 const express = require('express');
 const router = express.Router();
+const apiUtils = require('./utils/apiUtils.js');
+const DB = require('../db/db.js');
+const db = new DB();
+const proteinCollName = 'daily-per-capita-protein-supply.csv';
 
-router.get('/', (req, res) => {
-  res.send('Hello from the protein route!');
+/**
+ * Middleware for validating the 'country' parameter in the route
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Next middleware function
+ * @param {string} country - The 'country' parameter from the URL
+ */
+router.param('country', (req, res, next, country) => {
+  // TODO: decode url (spaces are replaced with %20)
+  if (!apiUtils.containsOnlyLetters(country)) {
+    apiUtils.sendError(res, 400, 'The country name cannot contain numbers or special characters');
+    return;
+  }
+
+  req.params.country = country.toLowerCase();
+  next();
+});
+
+/**
+ * GET handler for getting the protein intake of a specific country
+ * TO BE UPDATED
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+router.get('/countries/:country', async (req, res) => {
+  const startYear = req.query.startYear;
+  const endYear = req.query.endYear;
+  try {
+    if (startYear && endYear) {
+      apiUtils.validateYear(res, startYear, 'startYear');
+      apiUtils.validateYear(res, endYear, 'endYear');
+      apiUtils.validateYearRange(res, startYear, endYear);
+    } 
+  } catch {
+    return;
+  }
+
+  const data = await db.readAllCountryData(proteinCollName, req.params.country);
+
+  if (!data.length) {
+    apiUtils.sendError(res, 404, `No data found for ${req.params.country}`);
+    return;
+  }
+
+  let results = data.sort((a, b) => a.year - b.year).map(row => { 
+    return { year : row.year, gdp : row.gdp };
+  });
+
+  results = apiUtils.filterByStartYear(startYear, results);
+  results = apiUtils.filterByEndYear(endYear, results);
+
+  const responseBody = {
+    country: data[0].country,
+    code: data[0].code,
+    results : results
+  };
+
+  apiUtils.sendData(res, 200, responseBody);
 });
 
 module.exports = router;
