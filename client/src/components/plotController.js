18fa936cbaf41f2e67a5d6230e928307d89820e0
X8DTU-F <thomas.proctor@dawsoncollege.qc.ca>
2023-11-10 14:09:45 -0500

    Display Protein VS Gdp with 2 api calls and 2 y-axis with plotly

--- client/src/components/plotController.js
+++ client/src/components/plotController.js
@@ -3,11 +3,18 @@ import PlotDisplay from './plotDisplay';
 
 const PlotController = (props) => {
   const [newData, setNewData] = useState([]);
+  const [country, setCountry] = useState([]);
   useEffect(() => {
     fetch('/api/v1/protein/countries/canada')
       .then(res => res.json())
       .then(data => setNewData(data['data']['results']));
 
+      
+    fetch('/api/v1/gdp/countries/canada')
+      .then(res => res.json())
+      .then(data => setCountry(data['data']['results']));
+
+    console.log(newData);
   }
   , []);
 
@@ -15,6 +22,7 @@ const PlotController = (props) => {
   let data = [];
   // implement new data into the plot
   if (newData.length !== 0) {
+    console.log(newData);
     data = [
       {
         x: newData.map(row => row.year),
@@ -22,11 +30,19 @@ const PlotController = (props) => {
         name: 'Protein consumption',
         type: 'scatter',
         marker: {color: 'red'},
+      },
+      {
+        x: country.map(row => row.year),
+        y: country.map(row => row.gdp / 1000),
+        name: 'GDP (in thousands)',
+        type: 'scatter',
+        yaxis: 'y2',
+        marker: {color: 'blue'},
       }
     ];
   }
   const layout = {  
-    title: 'Protein Consumption',
+    title: 'GDP vs Protein Consumption',
     xaxis: {
       title: 'Year',
       showgrid: false,
@@ -35,6 +51,12 @@ const PlotController = (props) => {
     yaxis: {
       title: 'Protein Consumption (g)',
       showline: false
+    },
+    yaxis2: {
+      title: 'GDP (in thousands)',
+      showline: false,
+      overlaying: 'y',
+      side: 'right'
     }
   };
 
