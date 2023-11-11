08c01a7bbf08d0cd7b3c9b27377433e5864545c7
X8DTU-F <thomas.proctor@dawsoncollege.qc.ca>
2023-11-11 14:05:37 -0500

    Fix plot controller to accept gdp OR protein or GDP AND Protein

--- client/src/components/plotController.js
+++ client/src/components/plotController.js
@@ -1,31 +1,7 @@
-import React, { useEffect, useState } from 'react';
 import PlotDisplay from './plotDisplay';
 
 const PlotController = ({ gdp, protein, title }) => {
-  let data = [];
-
-  if (protein) {
-    let newProtein = {
-      x: protein.map(row => row.year),
-      y: protein.map(row => row.gppd),
-      name: 'Protein consumption',
-      type: 'scatter',
-      marker: {color: 'red'},
-    };
-    data.push(newProtein);
-  }
-
-  if (gdp) {
-    let newGdp = {
-      x: gdp.map(row => row.year),
-      y: gdp.map(row => row.gdp),
-      name: 'GDP per capita',
-      type: 'scatter',
-      yaxis: 'y2',
-      marker: {color: 'blue'},
-    };
-    data.push(newGdp);
-  }
+  const data = [];
 
   const layout = {  
     title: title,
@@ -34,17 +10,40 @@ const PlotController = ({ gdp, protein, title }) => {
       showgrid: false,
       zeroline: false
     },
-    yaxis: {
+  };
+
+  if (protein) {
+    const newProtein = {
+      x: protein.map(row => row.year),
+      y: protein.map(row => row.gppd),
+      name: 'Protein consumption',
+      type: 'scatter',
+      marker: {color: 'red'},
+    };
+    data.push(newProtein);
+    layout.yaxis = {
       title: 'Protein Consumption (g)',
       showline: false,
-    },
-    yaxis2: {
+    };
+  }
+
+  if (gdp) {
+    const newGdp = {
+      x: gdp.map(row => row.year),
+      y: gdp.map(row => row.gdp),
+      name: 'GDP per capita',
+      type: 'scatter',
+      yaxis: 'y2',
+      marker: {color: 'blue'},
+    };
+    data.push(newGdp);
+    layout.yaxis2 = {
       title: 'GDP (in thousands)',
       showline: false,
       overlaying: 'y',
       side: 'right'
-    }
-  };
+    };
+  }
 
   return(
     <div>
