7182fe9b4c0750a7ec96e8d68ebac8a372170b4a
X8DTU-F <thomas.proctor@dawsoncollege.qc.ca>
2023-11-14 16:12:08 -0500

    Updating app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import MapFilters from './components/filters/map/MapFilters';
+import QuizComponent from './components/quiz/QuizComponent';
 
 function App() {
-  const years = [1990, 1991, 1992, 1993, 1994, 1995];
-  const validCountries = ['Canada', 'Ukraine', 'Mexico', 'Russia', 'England'];
   return (
     <div className="App">
-      <MapFilters years={years} validCountries={validCountries}/>
+      <QuizComponent />
     </div>
   );
 }
