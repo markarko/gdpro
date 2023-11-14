1e7eeb4eda0d857a208e2dce808d833106937951
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-11-14 16:40:34 -0500

    Implement the basic Map view

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import DataDisplay from './components/DataDisplay';
 import MontrealMap from './components/Map/Map';
 import ChartView from './components/chart/ChartView';
+import MapView from './components/Map/MapView';
 
 function App() {
   return (
     <div className="App">
       <MontrealMap position={[45.5, -73.6]}/>
-      <DataDisplay />
-      <ChartView />
+      {/* <ChartView /> */}
+      <MapView />
     </div>
   );
 }
---
+++ client/src/components/Map/MapView.js
@@ -0,0 +1,49 @@
+import MapFilters from '../filters/map/MapFilters';
+import { useEffect, useState } from 'react';
+
+export default function MapView() {
+  
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [validYears, setValidYears] = useState([]);
+  const [validCountries, setValidCountries] = useState([]);
+  
+  async function getJson(url) {
+    const response = await fetch(url);
+    return await response.json();
+  }
+
+  useEffect(() => {
+    async function fetchInitialData() {
+      const yearsUrl = '/api/v1/gdp/countries/ukraine';
+      const countriesUrl = '/api/v1/gdp/countries/all'; 
+    
+      const fetches = [getJson(yearsUrl), getJson(countriesUrl)];
+      const [yearsData, countriesData] = await Promise.all(fetches);
+          
+      setLoading(false);
+    
+      const years = yearsData.data.results.map(x => x.year); 
+      setValidYears(years);
+    
+      const countries = countriesData.data;
+      setValidCountries(countries);
+    }
+    fetchInitialData();
+  }, []);
+  
+  if (error) {
+    return <div>{error}</div>;
+  }
+  
+  if (loading) {
+    return <div></div>;
+  }
+  
+  return <div>
+    <MapFilters
+      years={validYears}
+      validCountries={validCountries}
+    />
+  </div>;
+}
