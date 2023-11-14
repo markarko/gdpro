d41ef2a3b53646b98bfc70952a6803ecea95ccdf
X8DTU-F <thomas.proctor@dawsoncollege.qc.ca>
2023-11-14 16:22:47 -0500

    Modify seeder for country lat and lon

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -1,20 +1,24 @@
 const {datasetToJson, seedDatabase} = require('./parsing.js');
 
 (async () => {
-  const filesNames = ['daily-per-capita-protein-supply.csv', 'gdp-per-capita-worldbank.csv'];
+  const filesNames = ['daily-per-capita-protein-supply.csv', 'gdp-per-capita-worldbank.csv',
+    'countries.csv'];
   for (const fileName of filesNames) {
     const data = await datasetToJson(fileName);
     const filteredData = data.filter(row => !row.country.includes('FAO')).map(row => {
       const rowCopy = Object.assign(row);
       rowCopy.country = row.country.toLowerCase();
-      rowCopy.year = Number(row.year);
+      if (rowCopy.year) {
+        rowCopy.year = Number(row.year);
       
-      if (row.gdp) {
-        rowCopy.gdp = Number(row.gdp);
+        if (row.gdp) {
+          rowCopy.gdp = Number(row.gdp);
+        }
+        if (row.gppd){
+          rowCopy.gppd = Number(row.gppd);
+        }
       }
-      if (row.gppd){
-        rowCopy.gppd = Number(row.gppd);
-      }
+      
 
       return row;
     });
