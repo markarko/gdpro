1f7152024a29d95ba2dff9808bff912025f4a033
X8DTU-F <thomas.proctor@dawsoncollege.qc.ca>
2023-11-14 18:08:21 -0500

    Add design to navbar, fix navbar css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import QuizComponent from './components/quiz/QuizComponent';
 import ReactDOM from 'react-dom/client';
+import NavBar from './components/navbar/navbar';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="Quiz" element={<QuizComponent />} />
       </Routes>
