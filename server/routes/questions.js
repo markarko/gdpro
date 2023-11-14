dfe791c3dd7b06e6885d9b7b276148460c1329e4
X8DTU-F <thomas.proctor@dawsoncollege.qc.ca>
2023-11-14 17:24:43 -0500

    Update so now automatically fetches questions instead of using stub

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -6,6 +6,12 @@
 const express = require('express');
 const router = express.Router();
 const apiUtils = require('./utils/apiUtils.js');
+const DB = require('../db/db.js');
+const db = new DB();
+const proteinCollName = 'daily-per-capita-protein-supply';
+const gdpCollName = 'gdp-per-capita-worldbank';
+const countriesCollName = 'countries';
+
 
 /**
  * GET handler for getting the protein intake of a specific country
@@ -15,38 +21,66 @@ const apiUtils = require('./utils/apiUtils.js');
  * @param {Object} res - Express response object
  */
 router.get('/random-questions/:number', async (req, res) => {
-  
   const questions = req.params.number;
   if (!questions) {
     apiUtils.sendError(res, 400, 'No questions specified');
     return;
   }
+  const countries = await db.readAll(countriesCollName);
+  let cGDP = '';
+  let cPro = '';
+  let country = '';
 
-  const responseBody = ({
-    questions: [
-      {'Question': 'Which of the following countries has the',
-        'QData': {'GDP': '7', 'Protein': '8', 'Lan': '9', 'Lon': '10'},
-        'Answers': ['Angola', '7', 'something else'],
-        'Correct': 'Angola'},
-      {'Question': 'Which of the following countries has the',
-        'QData': {'GDP': '7', 'Protein': '8', 'Lan': '9', 'Lon': '10'},
-        'Answers': ['To have fun', '7', 'something else'],
-        'Correct': 'To have fun'},
-      {'Question': 'Which of the following countries has the',
-        'QData': {'GDP': '7', 'Protein': '8', 'Lan': '9', 'Lon': '10'},
-        'Answers': ['To have fun', '7', 'something else'],
-        'Correct': 'To have fun'},
-      {'Question': 'Which of the following countries has the',
-        'QData': {'GDP': '7', 'Protein': '8', 'Lan': '9', 'Lon': '10'},
-        'Answers': ['To have fun', '7', 'something else'],
-        'Correct': 'To have fun'},
-      {'Question': 'Which of the following countries has the',
-        'QData': {'GDP': '7', 'Protein': '8', 'Lan': '9', 'Lon': '10'},
-        'Answers': ['To have fun', '7', 'something else'],
-        'Correct': 'To have fun'},
-    ]
-  });
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    country = countries[Math.floor(Math.random() * countries.length)];
+    // eslint-disable-next-line no-await-in-loop
+    cGDP = await db.readAllCountryData(gdpCollName, country.name.toLowerCase());
+    // eslint-disable-next-line no-await-in-loop
+    cPro = await db.readAllCountryData(proteinCollName, country.name.toLowerCase());
+
+    cGDP = cGDP.find(gdp => cPro.some(pro => pro.year === gdp.year));
+    cPro = cPro.find(pro => pro.year === cGDP.year);
+    if (cGDP && cPro) {
+      break;
+    }
+  }
+
   
+  const Lon = country.longitude;
+  const Lat = country.lattitude;
+
+  const otherCountries = [];
+  for (let i = 0; i < 2; i++) {
+    const otherCountry = countries[Math.floor(Math.random() * countries.length)];
+    otherCountries.push(otherCountry);
+  }
+  otherCountries.push(country);
+
+  // randomize the order of the countries
+  otherCountries.sort(() => Math.random() - 0.5);
+
+  const responseBody = {
+    'questions': [
+      {'Question': 'Which of the following countries has the',
+        'QData': {'GDP': cGDP.gdp, 'Protein': cPro.gppd, 'Lan': Lat, 'Lon': Lon, 'year': cGDP.year},
+        'Answers': [otherCountries[0].name, otherCountries[1].name, otherCountries[2].name],
+        'Correct': country.name},
+      {'Question': 'Which of the following countries has the',
+        'QData': {'GDP': cGDP.gdp, 'Protein': cPro.gppd, 'Lan': Lat, 'Lon': Lon, 'year': cGDP.year},
+        'Answers': [otherCountries[0].name, otherCountries[1].name, otherCountries[2].name],
+        'Correct': country.name},
+      {'Question': 'Which of the following countries has the',
+        'QData': {'GDP': cGDP.gdp, 'Protein': cPro.gppd, 'Lan': Lat, 'Lon': Lon, 'year': cGDP.year},
+        'Answers': [otherCountries[0].name, otherCountries[1].name, otherCountries[2].name],
+        'Correct': country.name},
+      {'Question': 'Which of the following countries has the',
+        'QData': {'GDP': cGDP.gdp, 'Protein': cPro.gppd, 'Lan': Lat, 'Lon': Lon, 'year': cGDP.year},
+        'Answers': [otherCountries[0].name, otherCountries[1].name, otherCountries[2].name],
+        'Correct': country.name},
+    ]
+  };
+
   apiUtils.sendData(res, 200, responseBody);
 });
 
