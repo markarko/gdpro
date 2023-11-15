8b67bd40ed4a7ae851edd56ef4c0aa3c24bf5397
Marko <markolitovchenko24@gmail.com>
2023-11-15 09:00:33 -0500

    Add appropriate semantic elements for footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,32 +1,32 @@
 import './Footer.css';
 
 export default function Footer() {
-  return <div className="Footer">
+  return <footer className="Footer">
     <div>
       <h3>Goal:</h3>
-      <div>Our goal is to provide an easy interface for visualizing the GDP all around the world,
+      <p>Our goal is to provide an easy interface for visualizing the GDP all around the world,
         dating from the 1990s. At the same time, we want to demonstrate the correlation between a
         country's GDP and its average daily amount of protein intake
-      </div>
+      </p>
     </div>
     
     <div>
       <h3>Attributions:</h3>
-      <div>Click
+      <p>Click
         <a href="https://ourworldindata.org/grapher/gdp-per-capita-worldbank"> here </a>
         to view the gdp dataset
-      </div>
-      <div>Click
+      </p>
+      <p>Click
         <a href="https://ourworldindata.org/grapher/daily-per-capita-protein-supply"> here </a>
         to view the protein dataset
-      </div>
+      </p>
     </div>
 
     <div>
       <h3>Developers:</h3>
-      <div>Marko Litovchenko</div>
-      <div>Thomas Proctor</div>
-      <div>Amir</div>
+      <p>Marko Litovchenko</p>
+      <p>Thomas Proctor</p>
+      <p>Amir</p>
     </div>
-  </div>;
+  </footer>;
 }
