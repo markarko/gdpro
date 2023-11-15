9df917c1ec2fcc35f1826959a5f621539cfb4138
X8DTU-F <thomas.proctor@dawsoncollege.qc.ca>
2023-11-15 10:37:07 -0500

    Added questions validation and unique questions

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -21,70 +21,74 @@ const countriesCollName = 'country';
  * @param {Object} res - Express response object
  */
 router.get('/random-questions/:number', async (req, res) => {
-  const questions = req.params.number;
-  if (!questions) {
+  const numQuestion = req.params.number;
+
+  // check if the number of questions is valid (between 1 and 10)
+  if (numQuestion < 1 || numQuestion > 10) {
+    apiUtils.sendError(res, 400, 'Invalid number of questions');
+    return;
+  }
+  const questions = [];
+  if (!numQuestion) {
     apiUtils.sendError(res, 400, 'No questions specified');
     return;
   }
-  const countries = await db.readAll(countriesCollName);
-  let cGDP = '';
-  let cPro = '';
-  let country = '';
 
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    // This isn't great coding style, I understand, this is just a quick fix
-    country = countries[Math.floor(Math.random() * countries.length)];
+  for (let i = 0; i < numQuestion; i++) {
     // eslint-disable-next-line no-await-in-loop
-    cGDP = await db.readAllCountryData(gdpCollName, country.name.toLowerCase());
-    // eslint-disable-next-line no-await-in-loop
-    cPro = await db.readAllCountryData(proteinCollName, country.name.toLowerCase());
-
-    try {
-      cGDP = cGDP.find(gdp => cPro.some(pro => pro.year === gdp.year));
-      cPro = cPro.find(pro => pro.year === cGDP.year);
-    } catch {
-      continue;
-    }
-    
-    if (cGDP && cPro) {
-      break;
-    }
+    const question = await generateQuestion();
+    questions.push(question);
   }
 
+  async function generateQuestion() {
+    const countries = await db.readAll(countriesCollName);
+    let cGDP = '';
+    let cPro = '';
+    let country = '';
   
-  const Lon = country.longitude;
-  const Lat = country.lattitude;
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      // This isn't great coding style, I understand, this is just a quick fix
+      country = countries[Math.floor(Math.random() * countries.length)];
+      // eslint-disable-next-line no-await-in-loop
+      cGDP = await db.readAllCountryData(gdpCollName, country.name.toLowerCase());
+      // eslint-disable-next-line no-await-in-loop
+      cPro = await db.readAllCountryData(proteinCollName, country.name.toLowerCase());
+  
+      try {
+        cGDP = cGDP.find(gdp => cPro.some(pro => pro.year === gdp.year));
+        cPro = cPro.find(pro => pro.year === cGDP.year);
+      } catch {
+        continue;
+      }
+      
+      if (cGDP && cPro) {
+        break;
+      }
+    }
+  
+    
+    const Lon = country.longitude;
+    const Lat = country.lattitude;
+  
+    const otherCountries = [];
+    for (let i = 0; i < 2; i++) {
+      const otherCountry = countries[Math.floor(Math.random() * countries.length)];
+      otherCountries.push(otherCountry);
+    }
+    otherCountries.push(country);
+  
+    // randomize the order of the countries
+    otherCountries.sort(() => Math.random() - 0.5);
 
-  const otherCountries = [];
-  for (let i = 0; i < 2; i++) {
-    const otherCountry = countries[Math.floor(Math.random() * countries.length)];
-    otherCountries.push(otherCountry);
+    return {'Question': 'Which of the following countries has the',
+      'QData': {'GDP': cGDP.gdp, 'Protein': cPro.gppd, 'Lan': Lat, 'Lon': Lon, 'year': cGDP.year},
+      'Answers': [otherCountries[0].name, otherCountries[1].name, otherCountries[2].name],
+      'Correct': country.name};
   }
-  otherCountries.push(country);
-
-  // randomize the order of the countries
-  otherCountries.sort(() => Math.random() - 0.5);
 
   const responseBody = {
-    'questions': [
-      {'Question': 'Which of the following countries has the',
-        'QData': {'GDP': cGDP.gdp, 'Protein': cPro.gppd, 'Lan': Lat, 'Lon': Lon, 'year': cGDP.year},
-        'Answers': [otherCountries[0].name, otherCountries[1].name, otherCountries[2].name],
-        'Correct': country.name},
-      {'Question': 'Which of the following countries has the',
-        'QData': {'GDP': cGDP.gdp, 'Protein': cPro.gppd, 'Lan': Lat, 'Lon': Lon, 'year': cGDP.year},
-        'Answers': [otherCountries[0].name, otherCountries[1].name, otherCountries[2].name],
-        'Correct': country.name},
-      {'Question': 'Which of the following countries has the',
-        'QData': {'GDP': cGDP.gdp, 'Protein': cPro.gppd, 'Lan': Lat, 'Lon': Lon, 'year': cGDP.year},
-        'Answers': [otherCountries[0].name, otherCountries[1].name, otherCountries[2].name],
-        'Correct': country.name},
-      {'Question': 'Which of the following countries has the',
-        'QData': {'GDP': cGDP.gdp, 'Protein': cPro.gppd, 'Lan': Lat, 'Lon': Lon, 'year': cGDP.year},
-        'Answers': [otherCountries[0].name, otherCountries[1].name, otherCountries[2].name],
-        'Correct': country.name},
-    ]
+    'questions': questions
   };
 
   apiUtils.sendData(res, 200, responseBody);
