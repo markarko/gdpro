461a719d1da8851908648df3588ad4ab368a310c
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-11-15 20:59:38 -0500

    Put protein unit tests inside a describe block

--- server/__tests__/apiProtein.test.js
+++ server/__tests__/apiProtein.test.js
@@ -12,19 +12,35 @@ jest.mock('../db/db', () => {
   return DB;
 });
 
-it('should get protein for a country', async () => {
-  const url = '/api/v1/protein/countries/Iran';
-  const response = await request(app).get(url).set('Accept', 'application/json');
-  expect(response.body).toEqual(
-    {'data': {
+describe('should get protein for a country', () => {
+  it('should return 200 for valid country', async () => {
+    const url = '/api/v1/protein/countries/Iran';
+    const response = await request(app).get(url).set({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }).send({
       'country':'Iran', 'code':'IRN', 'results':[{'year':'2021', 'gppd':'50'}]
-    }
     });
-  expect(response.statusCode).toEqual(200);
+    expect(response.statusCode).toEqual(200);
+  });
 });
 
-it('should return 400 since country is invalid', async () => {
-  const url = '/api/v1/protein/countries/Ir@n';
-  const response = await request(app).get(url).set('Accept', 'application/json');
-  expect(response.statusCode).toEqual(400);
+describe('should return 400 since country is invalid', () => {
+  it('should return 400 since country is invalid', async () => {
+    const url = '/api/v1/protein/countries/Ir@n';
+    const response = await request(app).get(url).set('Accept', 'application/json');
+    expect(response.statusCode).toEqual(400);
+  });
+});
+
+describe('GET /api/v1/protein/countries/top/1', () => {
+  test('responds with error', async () => {
+    const url = '/api/v1/protein/countries/top/1';
+    const response = await request(app).get(url);
+    expect(response.body).toEqual(
+      {'error':
+      'orderBy query parameter can be one of the following values: \'highest\', \'lowest\''}
+    );
+    expect(response.statusCode).toEqual(400);
+  });
 });
