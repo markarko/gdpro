7f33f52ac702550c9ca675aa88454cf3e8e0d952
Marko <markolitovchenko24@gmail.com>
2023-11-21 12:34:26 -0500

    Add missing jsdocs for api utils

--- server/routes/utils/apiUtils.js
+++ server/routes/utils/apiUtils.js
@@ -1,5 +1,5 @@
 /**
- * Checks if a string contains only alphabetic characters
+ * Checks if a string contains only alphabetic characters and spaces
  *
  * @param {string} str - The string to be checked for alphabetic characters
  * @returns {boolean} Returns true if the string contains only alphabetic characters,
@@ -33,12 +33,14 @@ function sendData(res, status, data) {
 }
 
 /**
- * Validates the order of start year and end year parameters
+ * Validates whether the start param is not greater than the end param 
  *
  * @param {Object} res - The express response object
- * @param {number} start - The start year parameter
- * @param {number} end - The end year parameter
- * @throws {Error} - If the start year parameter is greater than the end year parameter
+ * @param {number} start - The start parameter
+ * @param {number} end - The end parameter
+ * @param {string} startParamName - The name of the start param in the query
+ * @param {string} endParamName - The name of the end param in the query
+ * @throws {Error} - If the start parameter is greater than the end parameter
  */
 function validateRange(res, start, end, startParamName, endParamName) {
   if (start && end && start > end) {
@@ -51,14 +53,14 @@ function validateRange(res, start, end, startParamName, endParamName) {
 }
 
 /**
- * Filters an array of objects by the start GDP
+ * Filters an array of objects based on a minimum value for each object
  * 
- * @param {number} startGDP - The start GDP to filter by
+ * @param {number} start - The min value to filter by
  * @param {Array<Object>} arr - An array of objects to be filtered
- * Each object must have a 'gdp' property
+ * Each object must have a key property that matches the 3rd argument
+ * @param {string} key - The key of the objects by the value of which the array should be filtered
  * @returns {Array<Object>} - The filtered array
  */
-
 function filterByStartInt(start, arr, key) {
   if (!start) {
     return arr;
@@ -68,11 +70,12 @@ function filterByStartInt(start, arr, key) {
 }
 
 /**
- * Filters an array of objects by the end GDP
+ * Filters an array of objects based on a maximum value for each object
  * 
- * @param {number} end - The end GDP to filter by
+ * @param {number} end - The max value to filter by
  * @param {Array<Object>} arr - An array of objects to be filtered
- * Each object must have a 'gdp' property
+ * Each object must have a key property that matches the 3rd argument
+ * @param {string} key - The key of the objects by the value of which the array should be filtered
  * @returns {Array<Object>} - The filtered array
  */
 function filterByEndInt(end, arr, key) {
@@ -88,6 +91,7 @@ function filterByEndInt(end, arr, key) {
  * 
  * @param {Object} res - The express response object
  * @param {number} param - The parameter to validate
+ * @param {string} paramName - The parameter name in the query
  * @throws {Error} - If the parameter is not a positive number
  */
 
@@ -105,7 +109,6 @@ function validateIntParam(res, param, paramName) {
  * @param {string} countries - The countries parameter to validate
  * @throws {Error} - If the countries parameter is not a string
  */
-
 function validateCountries(validCountries, countries) {
   const returnCountries = [];
   for (let i = 0; i < countries.length; i++) {
@@ -116,6 +119,17 @@ function validateCountries(validCountries, countries) {
   return returnCountries;
 }
 
+/**
+ * General purpose function that returns default values for a range if 'start' or 'end'
+ * are not provided
+ * 
+ * @param {number} start - The start value for a range
+ * @param {number} end - The end value for a range
+ * @param {number} defaultStart - The default value to assign 'start' in case it's undefined
+ * @param {number} defaultEnd - The default value to assign 'end' in case it's undefined
+ * @returns {Array<number>} - The 'start' and 'end' numbers with default values if one of them
+ * is not provided
+ */
 function getDefaultParams(start, end, defaultStart, defaultEnd) {
   start = start ? start : defaultStart;
   end = end ? end : defaultEnd;
@@ -123,18 +137,55 @@ function getDefaultParams(start, end, defaultStart, defaultEnd) {
   return [start, end];
 }
 
+/**
+ * Gets the default values for a range of years
+ * 
+ * @param {number} startYear - The start year
+ * @param {number} endYear - The end year
+ * @returns {Array<number>} - The 'startYear' and 'endYear' numbers
+ * with default values if one of them is not provided
+ */
 function getDefaultYearParams(startYear, endYear) {
   return getDefaultParams(startYear, endYear, 1990, 2020);
 }
 
+/**
+ * Gets the default values for a range of gdp
+ * 
+ * @param {number} startGdp - The start gdp
+ * @param {number} endGdp - The end gdp
+ * @returns {Array<number>} - The 'startGdp' and 'endGdp' numbers
+ * with default values if one of them is not provided
+ */
 function getDefaultGdpParams(startGdp, endGdp) {
   return getDefaultParams(startGdp, endGdp, 1, 1000000);
 }
 
+/**
+ * Gets the default values for a range of protein
+ * 
+ * @param {number} startProtein - The start protein
+ * @param {number} endProtein - The end protein
+ * @returns {Array<number>} - The 'startProtein' and 'endProtein' numbers
+ * with default values if one of them is not provided
+ */
 function getDefaultProteinParams(startProtein, endProtein) {
   return getDefaultParams(startProtein, endProtein, 1, 500);
 }
 
+/**
+ * Get the gdp or protein intake for a specific country
+ *
+ * @param {Object} req - The express request object
+ * @param {Object} res - The express response object
+ * @param {Object} db - The database object containing db function queries
+ * @param {string} collName - The db collection name which to get the data from
+ * @param {dataType} dataType - Value for which to get the data. Either 'gdp' or 'gppd'
+ * @returns {object} 200 - An object containing the data for the specified year range and country
+ * @returns {object} 404 - If no data is found for that country and year range
+ * @throws {string} 400 - If the 'startYear' or 'endYear' params are not numbers or if 'startYear'
+ * is greater than the 'endYear'
+ */
 async function getDataSpecificCountry(req, res, db, collName, dataType){
   let startYear = req.query.startYear;
   let endYear = req.query.endYear;
@@ -172,6 +223,20 @@ async function getDataSpecificCountry(req, res, db, collName, dataType){
   sendData(res, 200, responseBody);
 }
 
+/**
+ * Get the growth / decline of gdp or protein intake for a specific country over the years
+ *
+ * @param {Object} req - The express request object
+ * @param {Object} res - The express response object
+ * @param {Object} db - The database object containing db function queries
+ * @param {string} collName - The db collection name which to get the data from
+ * @param {dataType} dataType - Value for which to get the data. Either 'gdp' or 'gppd'
+ * @returns {object} 200 - An object containing the variation of protein intake (in %)
+ * for the specified year range and country
+ * @returns {object} 404 - If no data is found for that country and year range
+ * @throws {string} 400 - If the 'startYear' or 'endYear' params are not numbers or if 'startYear'
+ * is greater than the 'endYear'
+ */ 
 async function getVariationSpecificCountry(req, res, db, collName, dataType) {
   let startYear = req.query.startYear;
   let endYear = req.query.endYear;
@@ -218,6 +283,20 @@ async function getVariationSpecificCountry(req, res, db, collName, dataType) {
   );
 }
 
+/**
+ * Get the x highest or lowest gdps or protein intakes across all countries
+ *
+ * @route GET /countries/top/:top
+ * @param {Object} req - The express request object
+ * @param {Object} res - The express response object
+ * @param {Object} db - The database object containing db function queries
+ * @param {string} collName - The db collection name which to get the data from
+ * @param {dataType} dataType - Value for which to get the data. Either 'gdp' or 'gppd'
+ * @returns {object} 200 - An object containing protein data for the specified year and sorting.
+ * @returns {object} 404 - If no data is found for that year (really means there is no data at all)
+ * @throws {string} 400 - If the 'year' param is not a number or the 'orderBy' param is not a valid
+ * value
+ */ 
 async function getTopCountries(req, res, db, collName, countryCollName, dataType) {
   const top = req.params.top;
   const year = req.query.year;
@@ -259,7 +338,6 @@ async function getTopCountries(req, res, db, collName, countryCollName, dataType
 
   sendData(res, 200, { results : results });
 }
-
 
 module.exports = {
   sendData,
