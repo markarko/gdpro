b2a20fe0903235fee2ff2a310f76a412df372a95
Marko <markolitovchenko24@gmail.com>
2023-11-23 11:25:05 -0500

    Add data type selection

--- client/src/components/filters/map/data_range/DataRangeFilter.js
+++ client/src/components/filters/map/data_range/DataRangeFilter.js
@@ -1,6 +1,8 @@
 import Select from '../select/Select';
 
 export default function DataRangeFilter({ years, setDataRangeFilter, disable }) {
+  const dataTypeOptions = ['gdp', 'protein'];
+
   const updateYear = e => {
     const year = e.target.value;
     if (years.includes(Number(year))) {
@@ -11,12 +13,26 @@ export default function DataRangeFilter({ years, setDataRangeFilter, disable }) 
     }
   };
 
+  const updateDataType = e => {
+    const dataType = e.target.value;
+    if (dataTypeOptions.includes(dataType)) {
+      setDataRangeFilter((prevFilters) => ({
+        ...prevFilters,
+        dataType,
+      }));
+    }
+  };
+
   return (
     <div className={disable ? 'DataRangeFilter disabled' : 'DataRangeFilter'} >
       <Select
         options={years}
         labelText="Select the year: "
         onChange={e => updateYear(e)} />
+      <Select
+        options={dataTypeOptions}
+        labelText="Select minimum and maximum values for "
+        onChange={e => updateDataType(e)} />
     </div>
   );
 }
