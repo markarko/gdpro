fcd7dc30c8f7d8259d4001f34ae1777f94078f52
X8DTU-F <thomas.proctor@dawsoncollege.qc.ca>
2023-11-27 15:21:07 -0500

    Add country variation filter component

---
+++ client/src/components/filters/chart/variation/CountryVariationFilter.js
@@ -0,0 +1,58 @@
+import '../../Filters.css';
+import Select from '../../map/select/Select';
+import YearSlider from '../slider/Slider';
+
+export default function CountryVariationFilter({
+  years,
+  validCountries,
+  basicFilters,
+  setCountryVariationFilter,
+  disable }) {
+
+  const select = ['gdp', 'protein'];
+
+  const updateFilter = e => {
+    const country = e.target.value;
+    if (validCountries.includes(country)) {
+      setCountryVariationFilter((prevFilters) => ({
+        ...prevFilters,
+        country,
+      }));
+    }
+  };
+
+  const updateValue = e => {
+    const value = e.target.value;
+    if (select.includes(value)) {
+      setCountryVariationFilter((prevFilter) => ({
+        ...prevFilter,
+        value,
+      }));
+    }
+  };
+
+  return (
+    <div className={disable ? 'disabled' : ''} style={{'display':'flex', 'flexDirection':'column'}}>
+      <Select
+        options={validCountries}
+        labelText="Select the country: "
+        onChange={e => updateFilter(e)} />
+      <Select
+        options={select}
+        labelText="Select the variable: "
+        onChange={e => updateValue(e)} />
+      <YearSlider
+        labelText="Start year:"
+        values={years}
+        basicFilters={basicFilters}
+        setBasicFilters={setCountryVariationFilter}
+        yearType="startYear" />
+      <YearSlider
+        labelText="End year:"
+        values={years}
+        basicFilters={basicFilters}
+        setBasicFilters={setCountryVariationFilter}
+        yearType="endYear" />
+    </div>
+  );
+}
