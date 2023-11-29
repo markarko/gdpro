5fb7f20fa735feff95079d0bd9572af7e950e912
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-11-28 21:42:37 -0500

    Add jsdocs for the TopCountriesFilter component

--- client/src/components/filters/map/top_countries/TopCountriesFilter.js
+++ client/src/components/filters/map/top_countries/TopCountriesFilter.js
@@ -2,11 +2,21 @@ import '../../Filters.css';
 import './TopCountriesFilter.css';
 import Select from '../select/Select';
 
+/**
+ * Renders a filter component for selecting top countries based on various criteria.
+ * @param {Object} setTopCountriesFilter - A function to update the top countries filter state.
+ * @param {boolean} disable - A flag to disable the filter component.
+ * @returns {JSX.Element} The rendered filter component.
+ */
 export default function TopCountriesFilter({ setTopCountriesFilter, disable }) {
   const topRange = [3, 4, 5, 6, 7, 8, 9, 10];
   const variations = ['highest', 'lowest'];
   const values = ['gdp', 'protein'];
 
+  /**
+   * Updates the top range value in the filter state.
+   * @param {Event} e - The change event.
+   */
   const updateTopRange = e => {
     const top = e.target.value;
     if (topRange.includes(Number(top))) {
@@ -17,6 +27,10 @@ export default function TopCountriesFilter({ setTopCountriesFilter, disable }) {
     }
   };
 
+  /**
+   * Updates the variation value in the filter state.
+   * @param {Event} e - The change event.
+   */
   const updateVariation = e => {
     const variation = e.target.value;
     if (variations.includes(variation)) {
@@ -27,6 +41,10 @@ export default function TopCountriesFilter({ setTopCountriesFilter, disable }) {
     }
   };
 
+  /**
+   * Updates the value in the filter state.
+   * @param {Event} e - The change event.
+   */
   const updateValue = e => {
     const value = e.target.value;
     if (values.includes(value)) {
