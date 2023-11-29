95c004d722021ec901e40b8bf1941a653799283e
Marko <markolitovchenko24@gmail.com>
2023-11-29 15:53:03 -0500

    Improve loading experience for all views

--- client/src/components/chart/ChartView.js
+++ client/src/components/chart/ChartView.js
@@ -1,9 +1,9 @@
 import './ChartView.css';
 import ChartFilters from '../filters/chart/ChartFilters';
 import PlotController from '../chart/PlotController';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-export default function ChartView() {
+export default function ChartView({ validYears, validCountries, error, setError }) {
   const dataLayout = {
     'data': {
       'country': '',
@@ -12,49 +12,11 @@ export default function ChartView() {
     }
   };
 
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
-
   const [gdp, setGdp] = useState(dataLayout);
   const [protein, setProtein] = useState(dataLayout);
 
-  const [validYears, setValidYears] = useState([]);
-  const [validCountries, setValidCountries] = useState([]);
-
   const [chartTitle, setChartTitle] = useState('Chart representingthe ' + 
   'gdp and daily protein intake of afghanistan');
-
-  useEffect(() => {
-    async function getJson(url) {
-      const response = await fetch(url);
-      return await response.json();
-    }
-    async function fetchInitialData() {
-      try{
-        const yearsUrl = '/api/v1/gdp/countries/ukraine';
-        const countriesUrl = '/api/v1/gdp/countries/all'; 
-  
-        const fetches = [getJson(yearsUrl), getJson(countriesUrl)];
-        const [yearsData, countriesData] = await Promise.all(fetches);
-      
-        const years = yearsData.data.results.map(x => x.year); 
-        setValidYears(years);
-  
-        const countries = countriesData.data;
-        setValidCountries(countries);
-      } catch(err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchInitialData();
-  }, []);
-
-  if (loading) {
-    return <div></div>;
-  }
 
   return <div className="ChartView">
     <PlotController
